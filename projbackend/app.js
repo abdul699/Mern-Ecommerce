b77375201d9a784e3794b658467294a800c8a2a0
Abdul <kabdulsalam6@gmail.com>
2020-12-20 12:45:50 +0530

    added middlewares

---
+++ projbackend/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+
+const authRoutes = require("./routes/auth.js");
+
+
+// DB connections
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+.then(() => {
+    console.log("DB CONNECTED")
+});
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+// Routes
+app.use("/api", authRoutes);
+
+// Port
+const port = process.env.PORT || 8000;
+
+// Starting server
+app.listen(port, () => {
+    console.log(`app is running at ${port}`);
+});
---
+++ projbackend/routes/auth.js
@@ -0,0 +1,8 @@
+const express = require("express");
+var router = express.Router();
+
+router.get("/signout", (req, res) => {
+    res.send("user signout")
+});
+
+module.exports = router;
